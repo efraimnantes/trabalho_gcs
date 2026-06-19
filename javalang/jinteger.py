e0470fa9af9dcd9b34a6c9174911b21f38035758
rudimar neves <r_neves@ufms.br>
2026-06-19 15:06:02 -0400

    feat: add JInteger simple conversions refs #12

--- javalang/jinteger.py
+++ javalang/jinteger.py
@@ -1,3 +1,11 @@
 class JInteger:
     def __init__(self, value: int):
         self.value = value
+    def intValue(self):
+        return self.value
+
+    def longValue(self):
+        return self.value
+
+    def floatValue(self):
+        return float(self.value)
--- tests/test_jinteger.py
+++ tests/test_jinteger.py
@@ -3,3 +3,9 @@ from javalang.jinteger import JInteger
 def test_jinteger_creation():
     value = JInteger(10)
     assert value.value == 10
+
+def test_simple_conversions():
+    jint = JInteger(42)
+    assert jint.intValue() == 42
+    assert jint.longValue() == 42
+    assert jint.floatValue() == 42.0
