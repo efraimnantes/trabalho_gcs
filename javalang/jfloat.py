2569c703ac497f636d05180832eacde81bcfc083
thigas <thiago.nogueira@ufms.br>
2026-06-19 15:25:42 -0300

    feat: add JFloat numeric conversions refs #31

--- javalang/jfloat.py
+++ javalang/jfloat.py
@@ -1,3 +1,13 @@
 class JFloat:
-    def __init__(self, value: int):
-        self.value = value
+    def __init__(self, value):
+        self.value = float(value)
+
+    def intValue(self):
+        return int(self.value)
+
+    def longValue(self):
+        return int(self.value)
+
+    def byteValue(self):
+        # Simula o comportamento do byte em Java (limites de -128 a 127)
+        return (int(self.value) + 128) % 256 - 128
