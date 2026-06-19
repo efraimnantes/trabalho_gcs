1ed8a4aa8d522a04f7e410aca44a85f2aaeb4b84
thigas <thiago.nogueira@ufms.br>
2026-06-19 15:58:12 -0300

    feat: add JFloat IEEE checks refs #33

--- javalang/jfloat.py
+++ javalang/jfloat.py
@@ -1,3 +1,15 @@
+import math
+
 class JFloat:
-    def __init__(self, value: int):
-        self.value = value
+    def __init__(self, value):
+        self.value = float(value)
+
+    def isNaN(self):
+        return math.isnan(self.value)
+
+    def isInfinite(self):
+        return math.isinf(self.value)
+
+    @staticmethod
+    def isFinite(f):
+        return math.isfinite(float(f))
