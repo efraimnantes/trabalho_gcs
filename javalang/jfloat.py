f5646451e92e822d2b2cfad3e2593a600f845c80
thigas <thiago.nogueira@ufms.br>
2026-06-19 15:10:52 -0300

    feat: add JFloat constants and constructor refs #28

--- javalang/jfloat.py
+++ javalang/jfloat.py
@@ -1,3 +1,12 @@
 class JFloat:
-    def __init__(self, value: int):
-        self.value = value
+    MAX_VALUE = 3.4028235e38
+    MIN_VALUE = 1.4e-45
+    MIN_NORMAL = 1.17549435e-38
+    POSITIVE_INFINITY = float('inf')
+    NEGATIVE_INFINITY = float('-inf')
+    NaN = float('nan')
+    SIZE = 32
+    BYTES = 4
+
+    def __init__(self, value):
+        self.value = float(value)
